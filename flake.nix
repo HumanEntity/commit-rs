21d576a1c46b6fe5722a6ad157fd72ef1da21235
HumanEntity <human_entity@icloud.com>
2024-06-23 17:04:59 +0200

    refactor(flake.nix): Better structure

--- flake.nix
+++ flake.nix
@@ -12,19 +12,23 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        rustVersion = pkgs.rust-bin.stable."1.79.0".default;
+
+        packageName = "commit-rs";
+        packageVersion = "0.1.0";
+
+        rustVersion = "1.79.0";
+        rustToolchain = pkgs.rust-bin.stable.${rustVersion}.default;
 
         rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustVersion;
-          rustc = rustVersion;
+          cargo = rustToolchain;
+          rustc = rustToolchain;
         };
 
         myRustBuild = rustPlatform.buildRustPackage {
-          pname =
-            "commit-rs";
-          version = "0.1.0";
+          pname = packageName;
+          version = packageVersion;
           buildInputs = [pkgs.openssl pkgs.libgit2 pkgs.darwin.apple_sdk.frameworks.Security];
-          src = ./.; # the folder with the cargo.toml
+          src = ./.;
 
           cargoLock.lockFile = ./Cargo.lock;
         };
@@ -33,7 +37,7 @@
         defaultPackage = myRustBuild;
         devShell = pkgs.mkShell {
           buildInputs =
-            [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
+            [ (rustToolchain.override { extensions = [ "rust-src" ]; }) ];
         };
       });
 }
